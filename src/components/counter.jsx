8683ebfa64e3ac0a57579c13308dd6cb49e848ff
xf <alekssandro.barbosa@ccc.ufcg.edu.br>
2018-08-15 07:32:09 -0300

    yt videos module + styles as a state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,40 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
     imageUrl:
       "https://media.gettyimages.com/photos/studio-shot-of-young-beautiful-woman-picture-id610576696"
   };
 
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+
     return (
-      <React.Fragment>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-      </React.Fragment>
+      <div>
+        <span style={{ fontSize: 30 }} className={classes}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Ke90Tje7VS0?start=2636"
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+        />
+      </div>
     );
   }
   formatCount() {
     const { count } = this.state;
-    return this.state.count == 0 ? "Zero" : count;
+    return this.state.count === 0 ? "Zero" : count;
   }
 }
