69b4a9fdf803b53adb7cd3b018800067a5c2352b
xf <alekssandro.barbosa@ccc.ufcg.edu.br>
2018-08-15 21:21:04 -0300

    react is easy

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,67 +1,32 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  constructor() {
-    super();
-  }
-
-  handleIncrement = () => {
-    this.count;
+  handleIncrement = product => {
     this.setState({ count: this.state.count + 1 });
   };
 
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-    /* imageUrl:
-      "https://media.gettyimages.com/photos/studio-shot-of-young-beautiful-woman-picture-id610576696"
-  */
-  };
-
-  styles = {
-    fontSize: 55,
-    fontWeight: "bold"
+    count: 0
   };
 
   render() {
     return (
       <div>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>
-              {" "}
-              <span
-                style={{ fontSize: 30 }}
-                className={this.getBadgeColorState()}
-              >
-                {this.formatCount()}
-              </span>
-              <button
-                onClick={this.handleIncrement}
-                className="btn btn-secondary btn-sm"
-              >
-                Increment
-              </button>
-            </li>
-          ))}
-        </ul>
-
-        <iframe
-          title="learnReact"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/Ke90Tje7VS0?start=2636"
-          frameBorder="0"
-          allow="encrypted-media"
-          allowFullScreen
-        />
+        <span className={this.getBadgeColorState()}>{this.formatCount()}</span>
+        <button
+          onClick={product => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
+
   getBadgeColorState() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
+    return (
+      "badge m-2 badge-" + (this.state.count === 0 ? "warning" : "primary")
+    );
   }
 
   formatCount() {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
+import Footer from "./components/footer";
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("header"));
+ReactDOM.render(<Counters />, document.getElementById("counter"));
+ReactDOM.render(<Footer />, document.getElementById("footer"));
 registerServiceWorker();
